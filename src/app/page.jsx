2996babdb5c3d646525ef60a2816ba99833406e4
Mohammed Alakhras <31374510+mohammedalakhras@users.noreply.github.com>
2023-10-05 06:15:41 +0000

    Init Push

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
 "use client"
 import React from 'react'
-
-import styles from './page.module.css'
+// import styles from './page.module.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import { client } from "@gradio/client";
+import Image from 'next/image';
+import img from "../../public/img.jpg"
 
 export default function App() {
   async function Connect() {
@@ -14,14 +15,22 @@ export default function App() {
   }
 
   return (
-    <div className="App">
+    <div className="container center" style={{ textAlign: "center" ,fontSize:20}}  >
       <h1>Telegram API</h1>
 
-      <button
+      <div style={{  border: "2px solid #ccc",
+borderRadius: '50%', overflow: 'hidden',width:"250px",height:"250px",margin:"0 auto"}} >
+<Image src={img}
+width={250}
+height={250}
+alt="img"
+/>
+</div>
+       <button
         type="button"
         className="btn btn-outline-success bt-lg"
         onClick={Connect}
-      >
+      ><br />
         Activate
       </button>
     </div>
