3ca13edc75f451998fa6bcd31a7a3d0ba16ca71e
Mohammed Alakhras <31374510+mohammedalakhras@users.noreply.github.com>
2023-10-05 15:18:37 +0000

    second Push

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 //  import './globals.css'
 // import 'bootstrap/dist/css/bootstrap.css';
+import Script from 'next/script';
 
 import { Montserrat } from 'next/font/google'
 const bodyFont = Montserrat({ subsets: ['latin'] ,weight: ['400']})
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-  
+   <Script
+      src="https://polyfill.io/v3/polyfill.min.js?features=IntersectionObserver"
+      strategy="beforeInteractive"
+    />
       <body className={bodyFont.className}>
         
         <div className='container'>
